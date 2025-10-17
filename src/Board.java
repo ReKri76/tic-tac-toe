488fe4eb881694b34812e11ec29dc65151bd4fd2
Anton <anch85072@gmail.com>
2025-10-18 01:20:57 +0300

    Immmmmput

--- src/Board.java
+++ src/Board.java
@@ -1,3 +1,21 @@
 public class Board {
     private int[][] data = new int[3][3];
+    void print() {
+        for(int i = 0; i < 3; ++i) {
+            System.out.print(" | ");
+
+            for(int j = 0; j < 3; ++j) {
+                switch (this.data[i][j]) {
+                    case 0 -> System.out.print(' ');
+                    case 1 -> System.out.print('X');
+                    case 2 -> System.out.print('O');
+                }
+
+                System.out.print(" | ");
+            }
+
+            System.out.println();
+        }
+
+    }
 }
